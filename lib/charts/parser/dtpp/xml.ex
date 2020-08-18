ed1166527a01785b1bf47e7829701f5756d40ffb
Geoffrey Lessel <geo@geoffreylessel.com>
2020-08-17 21:05:27 -0500

    allow parsing a full string of xml

--- lib/charts/parser/dtpp/xml.ex
+++ lib/charts/parser/dtpp/xml.ex
@@ -1,4 +1,13 @@
 defmodule FaaCharts.Parser.DTPP.XML do
+  def parse_string(string) do
+    Saxy.parse_string(string, FaaCharts.Parser.DTPP.SaxyXMLHandler, [])
+  end
+
+  def parse_string!(string) do
+    {:ok, result} = parse_string(string)
+    result
+  end
+
   def parse_stream(xml_stream) do
     Saxy.parse_stream(xml_stream, FaaCharts.Parser.DTPP.SaxyXMLHandler, [])
   end
